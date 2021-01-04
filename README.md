f743fd8f5b245ef6dc635d52a8a60952890f1a49
Dennis Schulte <mail@dennis-schulte.de>
2021-01-04 16:39:55 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# 🚀 Terraform Examples
+
+This repository provides an example on how to roll out the steadybit Helm chart in your Kubernetes cluster with Terraform.
+
+## Usage
+
+```sh
+terraform init
+terraform apply -var agent_key="<replace-with-agent-key>"
+```
---
+++ main.tf
@@ -0,0 +1,24 @@
+# See https://registry.terraform.io/providers/hashicorp/helm/latest/docs for detailed instructions and further configuration options.
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+variable "agent_key" {
+  type = string
+  description = "The API key the agent uses."
+}
+
+# See https://registry.terraform.io/providers/hashicorp/helm/latest/docs for detailed instructions and further configuration options.
+resource "helm_release" "steadybit_helm_chart" {
+  name  = "steadybit-agent"
+  repository = "https://steadybit.github.io/helm-charts"
+  chart = "steadybit-agent"
+  namespace = "steadybit-agent"
+  create_namespace = true
+  set {
+    name  = "agent.key"
+    value = var.agent_key
+  }
+}
